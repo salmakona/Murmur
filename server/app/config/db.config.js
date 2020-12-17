2ff01a298f41ebb52126cc4d9075578f3d2c6079
salmakona <gugly2009@gmail.com>
2020-12-17 14:14:53 +0600

    db_script fixing

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    HOST: "192.168.64.2",
-    USER: "gugly",
-    PASSWORD: "gugly2009",
-    DB: "murmur"
+    HOST: "localhost",
+    PORT: 3307,
+    USER: "docker",
+    PASSWORD: "docker",
+    DB: "test"
 };
